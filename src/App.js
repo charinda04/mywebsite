786c97353e89c6ada94c2b3a5da0223c3018b7d8
charinda04 <charinda04@gmail.com>
2019-03-09 13:43:21 +0530

    countdown added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import Clock from "./components/clock/Clock";
 
 class App extends Component {
   render() {
+    const currentDate = new Date();
+    const year =
+      currentDate.getMonth() === 11 && currentDate.getDate() > 23
+        ? currentDate.getFullYear() + 1
+        : currentDate.getFullYear();
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>This page is under construction</p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.linkedin.com/in/charinda-dissanayake/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Contact Me
           </a>
-          <Clock />
+          <Clock date={`${year}-05-08T00:00:00`} />
         </header>
       </div>
     );
